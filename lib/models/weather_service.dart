402a31acd030d52c8db81afb713072f9eb5c5bf6
pacodemant <148328057+pacodemant@users.noreply.github.com>
2024-04-11 09:51:29 +0200

    API funktioniert im Grunde. Vor dem Einbinden der zweiten API (Ermittlung des Stadtnamens) müssen noch kleinere Fehler, die ich vor der zweiten API (für Ermittlung des Stadtnamens anhand der Geodaten) beheben muss.

---
+++ lib/models/weather_service.dart
@@ -0,0 +1,41 @@
+//weather_service.dart
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+//INF - fetchWeatherData() holt die Wetterdaten aus der JSON-Datei einer API
+// und stellt sie zur Verfügung — UND NUR DAS! Kein setState() oder ähnliches!
+
+Future<Map<String, dynamic>> fetchWeatherData() async {
+  final responseAPI = await http.get(
+    //INF - OBEN: API-Anfrage dauert, daher async&await
+
+/*INF Uri.parse()
+UNTEN: Die parse() Methode ist eine Funktion in Dart, die verwendet wird, um einen String in ein bestimmtes Format oder eine bestimmte Datenstruktur umzuwandeln. In diesem Fall wird Uri.parse() verwendet, um einen String in ein Uri-Objekt umzuwandeln. Hier: Uri.parse() wandelt die URL der Wetter-API, die als String vorliegt, in ein Uri-Objekt um und übergibt es an die  http.get()-Methode
+*/
+
+    Uri.parse(
+        'https://api.open-meteo.com/v1/forecast?latitude=48.783333&longitude=8.183333&current=temperature_2m,apparent_temperature,is_day,precipitation&timezone=Europe%2FBerlin&forecast_days=1'),
+  );
+
+//INF - Trifft Antwort ein, wird der Status der Antwort geprüft
+// wenn Anfrage erfolgreich (Code: 200), wandelt jsonDecode(response.body) den JSON-String in ein Map-Objekt um
+
+  if (responseAPI.statusCode == 200) {
+    // ignore: avoid_print
+    //print(response.body); //INF - Terminalausgabe: JSON-String 
+    final weatherDataMap = jsonDecode(responseAPI.body);
+    return {
+      'city': 'Stuttgart',
+      'temp': weatherDataMap['current']['temperature_2m'],
+      'perceivedTemp': weatherDataMap['current']['apparent_temperature'],
+      'rain': weatherDataMap['current']['precipitation'],
+      'daytime': weatherDataMap['current']['is_day'],
+      'longitude': weatherDataMap['longitude'],
+      'latitude': weatherDataMap['latitude'],
+    };
+  } else {
+    throw Exception(
+        'Hab\' Petrus nicht erreichen können. Fehlercode: ${responseAPI.statusCode}');
+  }
+}
--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -1,7 +1,9 @@
+//home_page.dart
 // ignore_for_file: prefer__ructors, prefer_const_constructors
 
-import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:wetter_app_asynchron/models/weather_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -13,13 +15,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  /* ------------------------------------------ Aufgabe 2 (Verwendung von JSON) */
-  /* -----------------------5.  Schreibe eine Methode des StatefulWidget-State, */
-  /* ----------------------------------- die die Werte aus dem JSON-String holt */
-  /* ------------------------------- und in die entsprechenden Variablen setzt. */
-
-  static const String jsonStringDummy = """
+//INF - Vom Doz zur Verfügung gestelte Dummy-Daten
+/*    static const String jsonStringDummy = """
  {
+     "latitude": 48.78,
      "longitude": 9.18,
      "current": {
          "time": "2024-01-12T11:45",
@@ -29,119 +28,141 @@ class _HomePageState extends State<HomePage> {
          "precipitation": 12.00
      }
  }
- """;
-
+ """;  */
+//
   /* -------------------------------------------------------------- json.decode */
-  //INF - holt die Werte aus dem JSON-String und schreibt sie in eine Map
-  Map<String, dynamic> weatherData = json.decode(jsonStringDummy);
 
-  //INF - die Variablen, die die Werte aus dem JSON-String bekommen sollen
-  String? city = 'Stuttgart';
-  double? temp;
-  double? perceivedTemp;
-  double? rain;
-  int? daytime;
+  //INF - Deklarierung der Variablen, die die Werte aus dem JSON-String bekommen sollen
+  String city = 'Stuttgart';
+  late double temp;
+  late double perceivedTemp;
+  late double rain;
+  late int daytime;
   //INF - folg. daytimeString ist eine if-Bed. => 1 = Tag, 0 = Nacht
-  late String daytimeString = daytime == 1 ? 'Tag' : 'Nacht';
-  double? longitude;
-  double? latitude;
+  String get daytimeString => daytime == 1 ? 'Tag' : 'Nacht';
+  late double longitude;
+  late double latitude;
+  bool isLoading = true;
   /* ---------------------------------------------------------------------- E */
 
-  //INF Aufgabe: Schreibe eine Methode des StatefulWidget-State, die die Werte aus dem
-  //JSON-String "jsonStringDummy" holt und in die entsprechenden Variablen setzt.
+  @override
+  void initState() {
+    super.initState();
+    fetchWeatherData().then(
+      (weatherDataMap) {
+        setState(
+          () {
+            city = weatherDataMap['city'];
+            temp = weatherDataMap['temp'];
+            perceivedTemp = weatherDataMap['perceivedTemp'];
 
-  void fetchWeatherData() {
-    city = 'Stuttgart';
-    temp = weatherData['current']['temperature_2m'];
-    perceivedTemp = weatherData['current']['apparent_temperature'];
-    rain = weatherData['current']['precipitation'];
-    daytime = weatherData['current']['is_day'];
-    daytimeString = daytime == 1 ? 'Tag' : 'Nacht';
-    longitude = weatherData['longitude'];
-    latitude = weatherData['latitude'];
+            if (kDebugMode) {
+              print('perceivedTemp wurde initialisiert: $perceivedTemp');
+            }
+            rain = weatherDataMap['rain'];
+            daytime = weatherDataMap['daytime'];
+            longitude = weatherDataMap['longitude'];
+            latitude = weatherDataMap['latitude'];
+            isLoading = false;
+          },
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          width: 250,
-          // height: 250,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Colors.black,
-              width: 1,
+    if (isLoading) {
+      return CircularProgressIndicator();
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Container(
+            width: 250,
+            // height: 250,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  decoration: BoxDecoration(),
+                  width: double.infinity,
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text('ppppp-Äbb'),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.amber,
+                  padding: EdgeInsets.all(10),
+                  child: Center(
+                    child: Text('Wedda-Äbb'),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Stadt: $city',
+                  style: TextStyle(
+                    fontSize: 18,
+                    height: 2.0,
+                    color: Colors.blueGrey,
+                  ),
+                ),
+                Text(
+                  'gefühlte Temp.: $perceivedTemp ',
+                  style: TextStyle(
+                    fontSize: 14,
+                    height: 2.0,
+                  ),
+                ),
+                Text(
+                  'Temperatur: $temp °C',
+                  style: TextStyle(
+                    fontSize: 14,
+                    height: 2.0,
+                  ),
+                ),
+                Text(
+                  'Niederschlag: $rain mm',
+                  style: TextStyle(
+                    fontSize: 12,
+                    height: 2.0,
+                  ),
+                ),
+                Text(
+                  'Tageszeit: $daytimeString',
+                  style: TextStyle(fontSize: 12, height: 2.0),
+                ),
+                Text(
+                  'Standort: lat: $latitude, lon: $longitude',
+                  style: TextStyle(fontSize: 12),
+                ),
+                SizedBox(height: 22),
+                ElevatedButton(
+                  onPressed: () {
+                    fetchWeatherData();
+                    setState(() {});
+                  },
+                  child: Text('Vorhersage updaten'),
+                ),
+                SizedBox(height: 22),
+              ],
             ),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: double.infinity,
-                color: Colors.amber,
-                padding: EdgeInsets.all(10),
-                child: Center(
-                  child: Text('Wedda-Äbb'),
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Text(
-                'Stadt: $city',
-                style: TextStyle(
-                  fontSize: 18,
-                  height: 2.0,
-                  color: Colors.blueGrey,
-                ),
-              ),
-              Text(
-                'gefühlte Temp.: $perceivedTemp ',
-                style: TextStyle(
-                  fontSize: 14,
-                  height: 2.0,
-                ),
-              ),
-              Text(
-                'Temperatur: $temp °C',
-                style: TextStyle(
-                  fontSize: 14,
-                  height: 2.0,
-                ),
-              ),
-              Text(
-                'Niederschlag: $rain mm',
-                style: TextStyle(
-                  fontSize: 12,
-                  height: 2.0,
-                ),
-              ),
-              Text(
-                'Tageszeit: $daytimeString',
-                style: TextStyle(fontSize: 12, height: 2.0),
-              ),
-              Text(
-                'Standort: lat: $latitude, lon: $longitude',
-                style: TextStyle(fontSize: 12),
-              ),
-              SizedBox(height: 22),
-              ElevatedButton(
-                onPressed: () {
-                  fetchWeatherData();
-                  setState(() {});
-                },
-                child: Text('Vorhersage updaten'),
-              ),
-              SizedBox(height: 22),
-            ],
-          ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
